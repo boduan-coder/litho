0b342978a7b417af1de73b0ea5e63c47481b46b1
Bo Duan <boduan402@gmail.com>
2020-05-20 16:42:08 -0700

    Reproduce the flexGrow warning message.

--- sample/src/main/java/com/facebook/samples/litho/playground/PlaygroundComponentSpec.java
+++ sample/src/main/java/com/facebook/samples/litho/playground/PlaygroundComponentSpec.java
@@ -17,21 +17,46 @@
 package com.facebook.samples.litho.playground;
 
 import android.graphics.Color;
+
 import com.facebook.litho.Column;
 import com.facebook.litho.Component;
 import com.facebook.litho.ComponentContext;
+import com.facebook.litho.Size;
+import com.facebook.litho.SizeSpec;
 import com.facebook.litho.annotations.LayoutSpec;
-import com.facebook.litho.annotations.OnCreateLayout;
+import com.facebook.litho.annotations.OnCreateLayoutWithSizeSpec;
 import com.facebook.litho.widget.Text;
+
+import static com.facebook.litho.SizeSpec.UNSPECIFIED;
 
 @LayoutSpec
 public class PlaygroundComponentSpec {
 
-  @OnCreateLayout
-  static Component onCreateLayout(ComponentContext c) {
-    return Column.create(c)
-        .backgroundColor(Color.WHITE)
-        .child(Text.create(c).textSizeSp(20).text("Playground sample"))
-        .build();
+  @OnCreateLayoutWithSizeSpec
+  static Component onCreateLayoutWithSizeSpec(ComponentContext c, int widthSpec, int heightSpec) {
+    final Component textComponent =
+        Text.create(c).textSizeSp(16).maxLines(1).text("Some text to measure asdfasfdasdfajskldfa;sdfjasdlfkasjdklfa;sldfajskdfajsd;falskjdfjlasdfaskjdfasdfasdfasdfasd").flexGrow(1.0f).backgroundColor(Color.GREEN).build();
+
+    Column.Builder column = Column.create(c).child(textComponent);
+
+    // UNSPECIFIED sizeSpecs will measure the text as being one line only,
+    // having unlimited width.
+    final Size textOutputSize = new Size();
+    textComponent.measure(
+        c,
+        SizeSpec.makeSizeSpec(0, UNSPECIFIED),
+        SizeSpec.makeSizeSpec(0, UNSPECIFIED),
+        textOutputSize);
+
+    // Assuming SizeSpec.getMode(widthSpec) == EXACTLY or AT_MOST.
+    final int layoutWidth = SizeSpec.getSize(widthSpec);
+    final boolean textFits = (textOutputSize.width <= layoutWidth);
+    if (!textFits) {
+      final Component textComponent2 =
+          Text.create(c).textSizeSp(16).textColor(Color.WHITE).text("Click me to expand text").backgroundColor(Color.BLUE).build();
+      column.child(textComponent2);
+    }
+
+    return column.build();
   }
 }
